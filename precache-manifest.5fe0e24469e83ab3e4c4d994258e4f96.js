9d884934e94618632835fdfa26077f1abe1236ed
Meraj-PC\Meraj Hossain <merajhossain29@gmail.com>
2019-01-28 23:13:24 +0600

    Updates

---
+++ precache-manifest.5fe0e24469e83ab3e4c4d994258e4f96.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8003f68d6db422b8143b",
+    "url": "/wedevsprojects/static/css/main.1fb8f9e2.chunk.css"
+  },
+  {
+    "revision": "8003f68d6db422b8143b",
+    "url": "/wedevsprojects/static/js/main.8003f68d.chunk.js"
+  },
+  {
+    "revision": "6c63252f4a0404a7ffa2",
+    "url": "/wedevsprojects/static/css/1.f8098f6a.chunk.css"
+  },
+  {
+    "revision": "6c63252f4a0404a7ffa2",
+    "url": "/wedevsprojects/static/js/1.6c63252f.chunk.js"
+  },
+  {
+    "revision": "c562e78938716797893e",
+    "url": "/wedevsprojects/static/js/runtime~main.c562e789.js"
+  },
+  {
+    "revision": "4c0bc28d119198f64460c6a9e133c3a7",
+    "url": "/wedevsprojects/static/media/wperpwhitelogo.4c0bc28d.svg"
+  },
+  {
+    "revision": "a2b3f9422f446f484f5ad2c2a387f440",
+    "url": "/wedevsprojects/static/media/homefeaturethumb.a2b3f942.png"
+  },
+  {
+    "revision": "249b00384c25bba92dfcecab5deb5477",
+    "url": "/wedevsprojects/static/media/Asset51.249b0038.svg"
+  },
+  {
+    "revision": "10c1ab92cb7f357c9b99ee3d15df045b",
+    "url": "/wedevsprojects/static/media/HRMIllustration1.10c1ab92.png"
+  },
+  {
+    "revision": "9d7407e56cfcbbe231bd2b726843379f",
+    "url": "/wedevsprojects/static/media/crm.9d7407e5.png"
+  },
+  {
+    "revision": "06461c6ea30bd0ca05ea66a9bed1b9cc",
+    "url": "/wedevsprojects/static/media/accounting.06461c6e.png"
+  },
+  {
+    "revision": "59e12d5e9bc19ed552e921971d111c55",
+    "url": "/wedevsprojects/static/media/projectmanagement.59e12d5e.png"
+  },
+  {
+    "revision": "d5aca580f2feab862373dfe6956a90a7",
+    "url": "/wedevsprojects/static/media/client-2.d5aca580.png"
+  },
+  {
+    "revision": "1e8e1df74edc79ef5e377276fc126367",
+    "url": "/wedevsprojects/static/media/BottomCTABG1.1e8e1df7.svg"
+  },
+  {
+    "revision": "2cc11dfc77c10a69e295c825a67e444c",
+    "url": "/wedevsprojects/index.html"
+  }
+];
